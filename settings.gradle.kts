9b09927599dd0a5f5d7502b18d20a57f42cca8d5
virtusupport <virtu.support@gmail.com>
2026-08-08 13:25:16 +0200

    added the code

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,15 @@
-
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "Virtu"
+include(":app")
