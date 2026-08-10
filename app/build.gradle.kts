db3efd30c5107afb621a74b283fe866a5fd9258d
virtusupport <virtu.support@gmail.com>
2026-08-10 12:29:36 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,4 +90,5 @@ dependencies {
     implementation("androidx.compose.material3:material3:1.1.2")
     implementation("androidx.compose.ui:ui-tooling-preview:1.5.4")
     implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.compose.material:material-icons-core:1.5.4")
 }
