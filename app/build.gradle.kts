cf95be3f4dce6b5168a42e75872ccb3232fe85a0
virtusupport <virtu.support@gmail.com>
2026-08-10 12:37:00 +0200

    added icon extended Implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,4 +91,5 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview:1.5.4")
     implementation("androidx.activity:activity-compose:1.8.0")
     implementation("androidx.compose.material:material-icons-core:1.5.4")
+    implementation("androidx.compose.material:material-icons-extended:1.5.4")
 }
